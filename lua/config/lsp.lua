88c3bfeec5501af8955f5edfd3b77310ebfabaf5
lMotol <motomasa_s@sslab.ics.keio.ac.jp>
2024-12-16 06:25:23 +0000

    ast-grep の formatter 設定

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -14,3 +14,28 @@ mason_lspconfig.setup_handlers({
         })
     end,
 })
+
+-- ast-grep LSP サーバーを設定
+lspconfig.ast_grep.setup({
+    cmd = { "ast-grep", "lsp" },
+    filetypes = { -- 対応するファイルタイプ
+        "c",
+        "cpp",
+        "rust",
+        "go",
+        "java",
+        "python",
+        "javascript",
+        "typescript",
+        "html",
+        "css",
+        "kotlin",
+        "dart",
+        "lua",
+    },
+    root_dir = lspconfig.util.root_pattern("sgconfig.yaml", "sgconfig.yml"), -- プロジェクトのルートディレクトリを自動判別
+    on_attach = function(client, bufnr)
+        -- ファイル保存時に自動でフォーマットを実行
+        vim.api.nvim_buf_set_option(bufnr, "formatexpr", "v:lua.vim.lsp.formatexpr()")
+    end,
+})
