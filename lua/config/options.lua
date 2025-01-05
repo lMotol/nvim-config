a3078841d1bdfa511adfee86530008f31f68daa1
moto <moto0123moto@gmail.com>
2025-01-05 11:49:55 +0900

    デフォルトで行の折り返しを有効化

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,6 +4,21 @@
 
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
+vim.opt.wrap = true
+
+-- 言語によって indent を変更する
+local filetype_tabstop = { typescriptreact = 2, typescript = 2 } -- filetype毎のインデント幅
+local usrftcfg = vim.api.nvim_create_augroup("UserFileTypeConfig", { clear = true })
+vim.api.nvim_create_autocmd("FileType", {
+    group = usrftcfg,
+    callback = function(args)
+        local ftts = filetype_tabstop[args.match]
+        if ftts then
+            vim.bo.tabstop = ftts
+            vim.bo.shiftwidth = ftts
+        end
+    end,
+})
 
 if os.getenv("TMUX") then
     vim.opt.termguicolors = true
