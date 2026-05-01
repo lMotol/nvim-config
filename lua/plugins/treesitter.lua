64949f5fe8a19bdc7053966a210447a79618fd9c
lMotol <motomasa_s@sslab.ics.keio.ac.jp>
2026-05-01 08:33:01 +0000

    fix: auto-install and update treesitter parsers
    Enable parser auto-installation, request the common language parsers explicitly, and run TSUpdate during plugin builds so new environments do not fail when a parser is missing.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,8 +2,23 @@ return {
     {
         "nvim-treesitter/nvim-treesitter",
         lazy = false,
+        build = ":TSUpdate",
         opts = function(_, opts)
-            vim.list_extend(opts.ensure_installed, { "cpp" })
+            opts.ensure_installed = opts.ensure_installed or {}
+            opts.auto_install = true
+            vim.list_extend(opts.ensure_installed, {
+                "bash",
+                "cpp",
+                "json",
+                "lua",
+                "markdown",
+                "markdown_inline",
+                "python",
+                "query",
+                "toml",
+                "vim",
+                "yaml",
+            })
         end,
     },
 }
